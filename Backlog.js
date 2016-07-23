48f22ef26572fc4cb433ebb7136cae39c1bb8184
Vinícius Paolozza <Vincius Paolozza>
2016-07-23 02:36:12 +0100

    First Commit

---
+++ Backlog.js
@@ -0,0 +1,6 @@
+//DONE:0 Criar uma estrutura de pastas do aplicativo e uma segunda camada de less.
+//DOING:0 Criar um modelo de ramificação Git flow(ramificação de integração).
+//TODO:0 [STORIE Otimizador de tarefas] minificação de css, js e html.
+//TODO:10 [STORIE Otimizador de tarefas] validação de js e html5.
+//TODO:20 [STORIE Otimizador de tarefas] gerador de build para deploy.
+//TODO:30 [STORIE Otimizador de tarefas] Criar um minificador de imagens em png, jpg e svg.
---
+++ README.md
@@ -0,0 +1,5 @@
+# proj-initial-angular
+
+## GIT flow
+Branch naming convention:
+- anything except: master, develop, release-*, or hotfix-*
